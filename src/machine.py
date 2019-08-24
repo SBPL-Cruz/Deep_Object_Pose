fdc77e10c707a401b5ce324b565d73c442b1dfa4
sbpl <aditya.kgp25@gmail.com>
2019-08-24 10:54:05 -0400

    debugged typo

--- src/machine.py
+++ src/machine.py
@@ -163,7 +163,7 @@ def joint_state_callback(data):
 
 #TODO: hardcode the threshold 1.25m and 0.75m
 def object_pose_callback(marker):
-    target_publiser = rospy.Publisher('/Object_Position', ar_track_alvar_msga::AlvarMarkers, queue_size=2)
+    target_publiser = rospy.Publisher('/Object_Position',AlvarMarkers, queue_size=2)
     #TODO: assuming x is the axis that's changing
     pst = marker.pose.pose.position.x;
 
@@ -182,7 +182,7 @@ def object_recognition_buffer(current_state, done_msg, rate):
                 break
             else:
                 #TODO: subscriber to the published topics
-                rospy.Subscriber("TODO", ar_track_alvar_msga::AlvarMarkers ,object_pose_callback)
+                rospy.Subscriber("TODO",AlvarMarkers ,object_pose_callback)
             rate.sleep()
         except KeyboardInterrupt:
             print('interrupted!')
