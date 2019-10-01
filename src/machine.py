9d1b878ad76e8bfd0daab76239f07d14bbc90e43
sbpl <aditya.kgp25@gmail.com>
2019-10-01 05:02:01 -0400

    saved temporary changes

--- src/machine.py
+++ src/machine.py
@@ -12,7 +12,6 @@ listening to an image topic and publishing poses.
 from __future__ import print_function
 import yaml
 import sys
-
 import numpy as np
 import cv2
 import tf
@@ -72,9 +71,101 @@ def wait_till_done(param, done_msg, rate):
         except KeyboardInterrupt:
             print('interrupted!')
 
+
+# object_pose = None
+transformed_object_pose = None
+def perch_callback(data):
+    from geometry_msgs.msg import Quaternion
+    from tf.transformations import quaternion_from_euler, quaternion_multiply
+
+    global transformed_object_pose
+
+    object_pose = data.pose
+    print("Detected Pose : {}".format(object_pose))
+    q_orig = [0,0,0,0]
+    q_orig[0] = object_pose.orientation.x
+    q_orig[1] = object_pose.orientation.y
+    q_orig[2] = object_pose.orientation.z
+    q_orig[3] = object_pose.orientation.w
+    print(q_orig)
+    # q_orig = Quaternion(q_orig[0], q_orig[1], q_orig[2], q_orig[3])
+    # q_rot_1 = quaternion_from_euler(0, 0, -math.pi/2)
+    q_rot_1 = quaternion_from_euler(0, 0, 0)
+    q_new = quaternion_multiply(q_rot_1, q_orig)
+    # q_rot_2 = quaternion_from_euler(0,math.pi/2, 0)
+    # q_new = quaternion_multiply(q_rot_2, q_new)
+
+    print(q_new)
+
+    pose = PoseStamped()
+    pose.header.stamp = rospy.Time.now()
+    pose.header.frame_id = data.header.frame_id
+    pose.pose.position.x = object_pose.position.x
+    pose.pose.position.y = object_pose.position.y
+    pose.pose.position.z = object_pose.position.z + 0.05
+
+    pose.pose.orientation.x = q_new[0]
+    pose.pose.orientation.y = q_new[1]
+    pose.pose.orientation.z = q_new[2]
+    pose.pose.orientation.w = q_new[3]
+    transformed_object_pose = pose
+
+
+    return
+
+def wait_till_done_perch(object_name, param, done_msg, rate):
+    from std_msgs.msg import String
+
+    pub = rospy.Publisher('/requested_object', String, queue_size=10)
+    publish_once = 0
+
+    rospy.Subscriber("/perch_pose", PoseStamped, perch_callback)
+    while not rospy.is_shutdown():
+        try:
+            if (rospy.get_param(param) == 1):
+                rospy.logwarn (done_msg)
+                break
+            if publish_once < 10:
+                pub.publish(String(object_name))
+                publish_once += 1
+            rate.sleep()
+        except KeyboardInterrupt:
+            print('interrupted!')
+
+def wait_till_done_perch_grasp(param, done_msg, rate):
+    dope_pose = rospy.Publisher('/dope/pose_world_object', PoseStamped, queue_size=10)
+    print("Publishing transformed pose until grasp")
+    print(transformed_object_pose)
+    while not rospy.is_shutdown():
+        try:
+            if (rospy.get_param(param) == 1):
+                rospy.logwarn (done_msg)
+                break
+            else:
+                dope_pose.publish(transformed_object_pose)
+            rate.sleep()
+        except KeyboardInterrupt:
+            print('interrupted!')
+
+def wait_till_done_perch_planner(param, done_msg, rate):
+    dope_pose = rospy.Publisher('/Grasps', PoseStamped, queue_size=10)
+    print("Publishing transformed pose until grasp")
+    print(transformed_object_pose)
+    while not rospy.is_shutdown():
+        try:
+            if (rospy.get_param(param) == 1):
+                rospy.logwarn (done_msg)
+                break
+            else:
+                dope_pose.publish(transformed_object_pose)
+            rate.sleep()
+        except KeyboardInterrupt:
+            print('interrupted!')
+
 def setup_variables():
     rospy.set_param("object_recognition_request", 0)
     rospy.set_param("object_recognition_done", 0)
+    rospy.set_param("perch_done", 0)
     rospy.set_param("table_segmentation_request", 0)
     rospy.set_param("table_segmentation_done", 0)
     rospy.set_param("grasp_request", 0)
@@ -210,7 +301,7 @@ if __name__ == "__main__":
             # sleep(30)
             # gain - 0.4 - angular, same for last state angular, max - 0.4
             rospy.set_param("hand_open_request", 1)
-            start_octomap_server()
+            # start_octomap_server()
 
             if '--custom_home_state' in myargv:
                 # Use the planner to go to a custom home location
@@ -222,7 +313,7 @@ if __name__ == "__main__":
 
                 pose = Pose()
                 # pose.position.x, pose.position.y, pose.position.z = (0.304, -0.507, 0.795)
-                pose.position.x, pose.position.y, pose.position.z = (0.4, -0.2, 0.9)
+                pose.position.x, pose.position.y, pose.position.z = (0.40, -0.18, 0.785)
                 pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w \
                     = (0.282, 0.155, 0.702, 0.635)
                 pose_in_map = get_transform_pose('base_footprint', 'map', pose)
@@ -231,9 +322,14 @@ if __name__ == "__main__":
                 go_to_custom_goal(pose_in_map, rate, "FULLBODY", controller=True)
             else:
 
-                rospy.logwarn ("Requesting Object")
-                rospy.set_param("object_recognition_request", 1)
-                wait_till_done("object_recognition_done", "Done Object", rate)
+                if '--use_perch' not in myargv:
+                    rospy.logwarn ("Requesting Object")
+                    rospy.set_param("object_recognition_request", 1)
+                    wait_till_done("object_recognition_done", "Done Object", rate)
+                else:
+                    rospy.logwarn ("Requesting PERCH Object")
+                    wait_till_done_perch("004_sugar_box", "perch_done", "Done Perch", rate)
+
 
                 if '--include_table_pose' in myargv:
                     rospy.logwarn ("Requesting Table Segmentation")
@@ -242,7 +338,11 @@ if __name__ == "__main__":
 
                 rospy.logwarn ("Requesting Grasp")
                 rospy.set_param("grasp_request", 1)
-                wait_till_done("grasp_done", "Done Grasp", rate)
+                if '--use_perch' not in myargv:
+                    wait_till_done("grasp_done", "Done Grasp", rate)
+                # else:
+                #     wait_till_done_perch_grasp("grasp_done", "Done Grasp", rate)
+
                 if '--custom_home_state' in myargv:
                     # If custom home state was used the new goal state would be in this file
                     config_name = "walker_goal_custom_home.yaml"
@@ -257,7 +357,11 @@ if __name__ == "__main__":
                 rospy.logwarn ("Requesting Planner")
                 rospy.set_param("walker_planner_mode", "FULLBODY")
                 rospy.set_param("walker_planner_request", 1)
-                wait_till_done("walker_planner_done", "Done Planner", rate)
+                if '--use_perch' not in myargv:
+                    wait_till_done("walker_planner_done", "Done Planner", rate)
+                else:
+                    wait_till_done_perch_planner("walker_planner_done", "Done Planner", rate)
+
                 rospy.set_param("walker_planner_done", 0)
 
                 rospy.logwarn ("Requesting Controller")
@@ -265,6 +369,24 @@ if __name__ == "__main__":
                 rospy.set_param("controller_request", 1)
                 wait_till_done("controller_done", "Done Controller", rate)
                 rospy.set_param("controller_done", 0)
+
+                if '--use_perch' in myargv:
+                    rospy.logwarn ("Requesting Hand Close")
+                    rospy.set_param("hand_close_request", 1)
+
+                    # rospy.logwarn ("Requesting Trac IK")
+                    # rospy.set_param("lift_request", 1)
+                    # sleep(2)
+                    # # go_to_custom_goal(pose_in_map, rate, "FULLBODY", controller=True)
+                    # rospy.set_param("trac_ik_request", 1)
+                    # wait_till_done("trac_ik_done", "Done Trac IK", rate)
+                    #
+                    # rospy.logwarn ("Requesting Controller")
+                    # rospy.set_param("skip_states", 0)
+                    # rospy.set_param("controller_request", 1)
+                    # wait_till_done("controller_done", "Done Controller", rate)
+                    # rospy.set_param("controller_done", 0)
+                    # rospy.set_param("trac_ik_done", 0)
 
                 if '--incl_trac_ik' in myargv:
                     # Rerun grasping in case base moved
